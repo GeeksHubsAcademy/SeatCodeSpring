2ee3b60b8605beadc9aedf1b2c2f0c7119714a23
Xavi <xavi@geekshubs.com>
2020-11-10 12:43:55 +0100

    configure test suite

--- src/test/java/com/javier/academiaseatcode/AcademiaseatcodeApplicationTests.java
+++ src/test/java/com/javier/academiaseatcode/AcademiaseatcodeApplicationTests.java
@@ -3,7 +3,7 @@ package com.javier.academiaseatcode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=AcademiaseatcodeApplication.class, webEnvironment =SpringBootTest.WebEnvironment.RANDOM_PORT )
 class AcademiaseatcodeApplicationTests {
 
     @Test
