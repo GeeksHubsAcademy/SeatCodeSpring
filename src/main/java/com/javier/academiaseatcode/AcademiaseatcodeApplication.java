b2649d53402f8e71ed6f0a621cc4e7f05f32d92d
Xavi <xavi@geekshubs.com>
2020-11-10 11:43:25 +0100

    first commit

---
+++ src/main/java/com/javier/academiaseatcode/AcademiaseatcodeApplication.java
@@ -0,0 +1,13 @@
+package com.javier.academiaseatcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaseatcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaseatcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javier/academiaseatcode/AcademiaseatcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javier.academiaseatcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaseatcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
