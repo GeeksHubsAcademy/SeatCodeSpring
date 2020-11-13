3a485bbf2892218e3051b000c77b7515a5cecfb8
Xavi <xavi@geekshubs.com>
2020-11-13 11:04:30 +0100

    added controller test

--- src/test/java/com/javier/academiaseatcode/teachers/ui/rest/TeacherControllerTest.java
+++ src/test/java/com/javier/academiaseatcode/teachers/ui/rest/TeacherControllerTest.java
@@ -1,7 +1,10 @@
 package com.javier.academiaseatcode.teachers.ui.rest;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.javier.academiaseatcode.AcademiaseatcodeApplication;
+import com.javier.academiaseatcode.teachers.domain.entities.Teacher;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AcademiaseatcodeApplication.class)
 @AutoConfigureMockMvc
@@ -42,9 +47,22 @@ public class TeacherControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void ShouldSameNameToCreateTeacher() throws Exception {
+        Teacher teacher = new Teacher("Prueba", "Prueba");
+        mvc.perform(post("/teachers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.toJson(teacher)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(teacher.getName()));
+    }
 
 
-
-
+    static byte[] toJson(Object object ) throws  Exception
+    {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
+        return gson.toJson(object).getBytes();
+    }
 
 }
